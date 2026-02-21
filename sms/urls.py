b80eb32fc3c04ab33ed04fb55bc28a7d28465735
Kenzo72026 <keno01071@gmail.com>
2026-02-21 23:16:09 +0300

    Update urls.py

--- sms/urls.py
+++ sms/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('message/<int:pk>/', views.detail_message, name='detail_message'),
     path('message/<int:pk>/supprimer/', views.supprimer_message, name='supprimer_message'),
-    path('message/<int:pk>/verifier/', views.verifier_transaction, name='verifier_transaction'),
     path('api/messages/', views.api_messages, name='api_messages'),
     path('ping/', views.ping, name='ping'),
 ]
